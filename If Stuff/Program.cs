e84ef4bad19e3ba9092ac3f55a0a849a2b432fab
Emil Rotmark <emil.rotmark@elev.ga.ntig.se>
2024-11-07 10:18:54 +0100

    do you ever just forget to initialise a repository i know i do

---
+++ If Stuff/Program.cs
@@ -0,0 +1,24 @@
+﻿bool isRight = false;
+string playerAnswer;
+int randomInteger = Random.Shared.Next(100) + 1;
+int invalidGuesses = 0;
+while (!isRight){
+    Console.WriteLine("guess a number, 1-100");
+    int playerInt;
+    do{
+        if (invalidGuesses > 0){
+            Console.WriteLine("Ehh wrong you're wrong you didn't type a number dummy");
+        }
+        playerAnswer = Console.ReadLine();
+        if (int.TryParse(playerAnswer, out playerInt)){
+            invalidGuesses = 0;
+        }
+    } while(!int.TryParse(playerAnswer, out playerInt));
+    if (playerInt == randomInteger){
+        Console.WriteLine("Ding ding ding you are winner you are awesome I love you");
+        isRight = true;
+    }
+    else{
+        Console.WriteLine("You're WRONG and STINKY and I HATE YOU");
+    }
+}
