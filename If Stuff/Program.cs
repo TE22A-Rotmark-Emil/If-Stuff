363c905133f7c8e1252267bc072a2b8c0b7debf9
Emil Rotmark <emil.rotmark@elev.ga.ntig.se>
2024-11-07 10:24:50 +0100

    okay im done

--- If Stuff/Program.cs
+++ If Stuff/Program.cs
@@ -1,9 +1,10 @@
 ﻿bool isRight = false;
 string playerAnswer;
-int randomInteger = Random.Shared.Next(100) + 1;
+int maxInt = 100;
+int randomInteger = Random.Shared.Next(maxInt) + 1;
 int invalidGuesses = 0;
 while (!isRight){
-    Console.WriteLine("guess a number, 1-100");
+    Console.WriteLine($"guess a number, 1-{maxInt}");
     int playerInt;
     do{
         if (invalidGuesses > 0){
@@ -13,12 +14,19 @@ while (!isRight){
         if (int.TryParse(playerAnswer, out playerInt)){
             invalidGuesses = 0;
         }
+        else{
+            invalidGuesses++;
+        }
     } while(!int.TryParse(playerAnswer, out playerInt));
     if (playerInt == randomInteger){
         Console.WriteLine("Ding ding ding you are winner you are awesome I love you");
         isRight = true;
     }
+    else if (playerInt < randomInteger){
+        Console.WriteLine("who do you think I AM? you think I'd pick such a TINY and INSIGNIFICANT number??? shaking my head");
+    }
     else{
-        Console.WriteLine("You're WRONG and STINKY and I HATE YOU");
+        Console.WriteLine("WHATTT??+ CAN ANYONE EVEN COUNT THAT HIGH?!??? no wrong number it is smaller");
     }
 }
+Console.ReadLine();
